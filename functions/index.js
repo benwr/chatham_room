215b8eaef82141a490e7efb7788ca31b743f17db
Ben Weinstein-Raun <b@w-r.me>
2021-09-02 01:34:00 -0700

    more complete email sending function

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,28 @@
 const functions = require("firebase-functions");
-const axios = require("axios");
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const mailgun = require("mailgun-js")({apiKey: functions.config().mailgun.key, domain: "chathamroom.com"});
 
 exports.sendInvitation = functions.https.onCall((data, context) => {
-  const api_key = functions.config().mailgun.key;
-  // if (!context.auth.email) {
-  //   throw new functions.https.HttpsError("unauthorized", "Email not in auth token");
-  // }
-  return axios.post("https://api:" + api_key + "@api.mailgun.net/v3/chathamroom.com/messages", {
-    from: "staff@chathamroom.com",
-    to: data.invitee || "",
-    subject: "You've been invited to a Chatham Room",
-    template: "invitation-template",
-    "v:sender_email": context.auth.email || "",
-    "v:invitee_email": data.invitee || "",
-    "v:room_id": data.room_id || "",
-    "v:room_name": data.room_name || "",
-  }).then(res => {
-    if (res.status !== 200) {
-      throw new functions.https.HttpsError("unknown", "Mailgun gave a non-200 response code");
-    } else {
-      return {}
-    }
-  }).catch(err => {
-      throw new functions.https.HttpsError("unknown", "An error was encountered when attempting to talk to Mailgun");
-  });
+  if (!context.auth || !context.auth.email) {
+    throw new functions.https.HttpsError("unauthorized", "Email not in auth token");
+  }
+  var sendings = [];
+  for (const email of data.targets) {
+    const req = {
+      from: "Chatham Room Staff <staff@chathamroom.com>",
+      to: email,
+      subject: "You have been invited to a Chatham Room",
+      template: "invitation-template",
+      "v:sender_email": context.auth.email,
+      "v:invitee_email": email,
+      "v:room_id": data.room_id,
+      "v:room_name": data.room_name,
+    };
+    sendings.push(mailgun.messages().send(req, (error, body) => {
+      if (error) {
+        throw new functions.https.HttpsError("unknown", "An error was encountered when attempting to talk to Mailgun: " + body);
+      }
+      return {};
+    }));
+  }
+  return Promise.allSettled(sendings);
 });
