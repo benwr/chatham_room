0dc68b5fd27e880833c623e359a9ddc5b9590fef
Ben Weinstein-Raun <b@w-r.me>
2021-09-01 21:14:52 -0700

    move firebase to different location

---
+++ utils/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3NsMLi-R8d8S-F__VusmYGONkKTk9xr8",
+  authDomain: "chatham-room.firebaseapp.com",
+  projectId: "chatham-room",
+  storageBucket: "chatham-room.appspot.com",
+  messagingSenderId: "678062458841",
+  appId: "1:678062458841:web:337f7c4b77bc78e062bbc8",
+  measurementId: "G-G77SP8E9K7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+
