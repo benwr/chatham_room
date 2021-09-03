e788ac17745cc6fa6d27cb60d2b1d0b2f2a79da4
Ben Weinstein-Raun <b@w-r.me>
2021-09-03 14:58:20 -0700

    add uncloak option

--- src/Room.js
+++ src/Room.js
@@ -28,8 +28,9 @@ const MAX_DEPTH=5;
 class Room extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {"ROOT": ""};
+    this.state = {"ROOT": {content: "", uncloaked: false}, "email": ""};
     this.handleTyping = this.handleTyping.bind(this);
+    this.handleUncloak = this.handleUncloak.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReply = this.handleReply.bind(this);
   }
@@ -37,6 +38,7 @@ class Room extends React.Component {
   componentDidMount() {
     onAuthStateChanged(this.props.auth, (user) => {
       if (user && this.props.room.emails && this.props.room.emails.includes("," + user.email + ",")) {
+        this.setState({"email": user.email})
         var visited_ref = ref(this.props.db, "users/" + user.uid + "/visited/" + this.props.room_id);
         set(visited_ref, {
           name: this.props.room.name,
@@ -48,9 +50,23 @@ class Room extends React.Component {
   }
 
   handleTyping(thread_id, content) {
-    this.setState({
-      [thread_id]: content
-    });
+    var newstate;
+    if (!this.state[thread_id]) {
+      newstate = {content: content, uncloaked: false};
+    } else {
+      newstate = {content: content, uncloaked: this.state[thread_id].uncloaked}
+    }
+    this.setState({[thread_id]: newstate});
+  }
+
+  handleUncloak(thread_id, value) {
+    var newstate;
+    if (!this.state[thread_id]) {
+      newstate = {content: "", uncloaked: false};
+    } else {
+      newstate = {content: this.state[thread_id].content, uncloaked: value}
+    }
+    this.setState({[thread_id]: newstate});
   }
 
   handleSubmit(thread_id) {
@@ -60,18 +76,27 @@ class Room extends React.Component {
     } else {
       new_ref = push(ref(this.props.db, "rooms/" + this.props.room_id + "/messages/" + thread_id + "/children"));
     }
-    set(new_ref, {
-      content: this.state[thread_id],
-      time: serverTimestamp(),
-      children: {}
-    });
-    var state_update = {[thread_id]: ""};
+    if (this.state[thread_id].uncloaked) {
+      set(new_ref, {
+        content: this.state[thread_id].content,
+        author: this.state.email,
+        time: serverTimestamp(),
+        children: {}
+      });
+    } else {
+      set(new_ref, {
+        content: this.state[thread_id].content,
+        time: serverTimestamp(),
+        children: {}
+      });
+    }
+    var state_update = {[thread_id]: {content: "", uncloaked: this.state[thread_id].uncloaked}};
     this.setState(state_update);
   }
 
   handleReply(thread_id) {
     this.setState({
-      [thread_id]: ""
+      [thread_id]: {content: "", uncloaked: false}
     });
   }
 
@@ -88,6 +113,7 @@ class Room extends React.Component {
             thread_id={k}
             replies={this.state}
             handleTyping={this.handleTyping}
+            handleUncloak={this.handleUncloak}
             handleSubmit={this.handleSubmit}
             handleReply={this.handleReply}
           />
@@ -102,10 +128,13 @@ class Room extends React.Component {
         <br />
         {messages}
         <ReplyForm
+          handleUncloak={this.handleUncloak}
           handleTyping={this.handleTyping}
           handleSubmit={this.handleSubmit}
           thread_id="ROOT"
-          content={this.state["ROOT"]} />
+          content={this.state["ROOT"].content}
+          uncloaked={this.state["ROOT"].uncloaked}
+      />
       </div>
   }
 
@@ -116,6 +145,7 @@ class ReplyForm extends React.Component {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTickBox = this.handleTickBox.bind(this);
   }
 
   componentDidMount() {
@@ -131,6 +161,10 @@ class ReplyForm extends React.Component {
     this.props.handleTyping(this.props.thread_id, event.target.value);
   }
 
+  handleTickBox(event) {
+    this.props.handleUncloak(this.props.thread_id, event.target.value);
+  }
+
   handleSubmit(event) {
     event.preventDefault();
     this.props.handleSubmit(this.props.thread_id);
@@ -141,20 +175,15 @@ class ReplyForm extends React.Component {
   }
 
   render() {
-    return React.createElement("form", {
-      className: "message-form",
-      onSubmit: this.handleSubmit
-    }, React.createElement("input", {
-      className: "message-input",
-      ref: (inputref) => {this.inputref = inputref},
-      id: this.props.thread_id,
-      type: "text",
-      value: this.props.content,
-      onChange: this.handleChange
-    }), React.createElement("button", {
-      className: "message-button",
-      type: "submit"
-    }, "Send"));
+    return (<form className="message-form" onSubmit={this.handleSubmit}>
+        <input className="message-input" ref={inputref => {this.inputref = inputref}} id={this.props.thread_id} type="text" value={this.props.content} onChange={this.handleChange} />
+        <button className="message-button" type="submit">Send</button>
+        <br />
+        <label>
+          Send Uncloaked:
+          <input type="checkbox" value={this.props.uncloaked} onChange={this.handleTickBox} />
+        </label>
+      </form>)
   }
 
 }
@@ -180,6 +209,13 @@ class Message extends React.Component {
     } else {
       stamp = <span className="timestamp" />
     }
+
+    var byline;
+    if (this.props.m.author) {
+      byline = <div className="byline">{this.props.m.author} @ {stamp}</div>
+    } else {
+      byline = <div className="byline">{stamp}</div>
+    }
     var reply_content;
     if (this.props.depth < MAX_DEPTH) {
       if (this.props.replies[this.props.thread_id] === undefined) {
@@ -190,10 +226,12 @@ class Message extends React.Component {
         }, "⮑ reply");
       } else {
         reply_content = React.createElement(ReplyForm, {
+          handleUncloak: this.props.handleUncloak,
           handleTyping: this.props.handleTyping,
           handleSubmit: this.props.handleSubmit,
           thread_id: this.props.thread_id,
-          content: this.props.replies[this.props.thread_id]
+          content: this.props.replies[this.props.thread_id].content,
+          uncloaked: this.props.replies[this.props.thread_id].uncloaked,
         });
       }
     } else {
@@ -218,7 +256,7 @@ class Message extends React.Component {
     }
 
     return <div className="message">
-      {stamp}
+      {byline}
       {this.props.m.content}
       <br />
       {replies}
